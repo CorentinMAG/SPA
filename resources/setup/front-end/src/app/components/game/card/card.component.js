66233231262fce5e2bc572f63ee74d47b6eeb873
CorentinMAG <cor.mag91@gmail.com>
2020-04-21 19:13:42 +0200

    step 3 terminé

--- resources/setup/front-end/src/app/components/game/card/card.component.js
+++ resources/setup/front-end/src/app/components/game/card/card.component.js
@@ -2,7 +2,6 @@
 
 (function() { // TODO Step 6 remove this closure
 
-    // TODO Step 3.1 create a class
     /* class CardComponent constructor */
     class CardComponent {
         // is this card flipped ?
@@ -16,9 +15,8 @@
 
             this._elt = document.getElementById('card-template').content.cloneNode(true).firstElementChild;
             this._imageElt = this._elt.querySelector('.card-wrapper');
-            // TODO Step 1: Change images location to ./card/assets/***.png
-            // TODO Step 3.2: use template literals
-            this._imageElt.querySelector('img.front-face').src = './card/assets/card-' + this._id + '.png';
+
+            this._imageElt.querySelector('img.front-face').src = `./card/assets/card-${this._id}.png`;
             this._imageElt.querySelector('img.back-face').src = './card/assets/back.png';
 
         }
@@ -38,38 +36,7 @@
 
     }
 
-    /* method CardComponent.getElement */
-    //CardComponent.prototype.getElement = getElement;
-
     // TODO Step 6 implement getTemplate() {}
-
-    /* method CardComponent.flip */
-    //CardComponent.prototype.flip = flip;
-
-    /* method CardComponent.equals */
-    //CardComponent.prototype.equals = equals;
-
-    //function getElement() {
-    //    return this._elt;
-    //}
-
-    //function flip() {
-    //    this._imageElt.classList.toggle('flip');
-    //    this._flipped = !this._flipped;
-    //}
-    //
-    //function equals(card) {
-    //    return card._id === this._id;
-    //}
-
-    /* CardComponent.get flipped() */
-    //Object.defineProperties(CardComponent.prototype, {
-    //    flipped: {
-    //        get: function() {
-    //            return this._flipped;
-    //        }
-    //    }
-    //});
 
     var environment = {
         api: {}
--- resources/setup/front-end/src/app/components/game/game.component.js
+++ resources/setup/front-end/src/app/components/game/game.component.js
@@ -7,157 +7,134 @@
         }
     };
 
-    // TODO Step 3.1 create a class
+    var params = parseUrl();
+
     /* class GameComponent constructor */
-    function GameComponent() {
+    class GameComponent {
         // gather parameters from URL
-        var params = parseUrl();
 
-        // save player name & game ize
-        this._name = params.name;
-        this._size = parseInt(params.size) || 9;
-        this._flippedCard = null;
-        this._matchedPairs = 0;
-    }
+        constructor() {
+            // save player name & game ize
+            this._name = params.name;
+            this._size = parseInt(params.size) || 9;
+            this._flippedCard = null;
+            this._matchedPairs = 0;
 
-    /* method GameComponent.init */
-    GameComponent.prototype.init = init;
+        }
+        init() {
+            this.fetchConfig((config) => {
+                this._config = config;
 
-    // TODO Step 6 implement getTemplate() {}
+                // create a card out of the config
+                this._cards = this._config.ids.map((id) => new CardComponent(id))
 
-    /* method GameComponent.start */
-    GameComponent.prototype.start = start;
+                this._boardElement = document.querySelector('.cards');
 
-    /* method GameComponent.fetchConfig */
-    GameComponent.prototype.fetchConfig = fetchConfig;
-
-    /* method GameComponent.gotoScore */
-    GameComponent.prototype.gotoScore = gotoScore;
-
-    /* method GameComponent._flipCard */
-    GameComponent.prototype._flipCard = _flipCard;
-
-    function init() {
-        // fetch the cards configuration from the server
-        this.fetchConfig((function(config) { // TODO Step 3.2: use arrow function
-            this._config = config;
-
-            // create a card out of the config
-            this._cards = []; // TODO Step 3.3: use Array.map()
-            for (let i in this._config.ids) {
-                this._cards[i] = new CardComponent(this._config.ids[i]);
-            }
-            this._boardElement = document.querySelector('.cards');
-
-            for (let i in this._cards) { // TODO Step 3.3: use Array.forEach()
-                (function() {
-                    let card = this._cards[i];
+                this._cards.forEach((card) => {
                     this._boardElement.appendChild(card.getElement());
                     card.getElement().addEventListener('click', function() {
                         this._flipCard(card)
-                    }.bind(this)); // TODO use arrow function.
-                }).bind(this)();
+                    }.bind(this));
+                })
+
+                this._cards.forEach((card) => {
+                    this._boardElement.appendChild(card.getElement());
+                    card.getElement().addEventListener('click', function() {
+                        this._flipCard(card)
+                    }.bind(this))
+                })
+
+                this.start();
+            });
+        }
+        start() {
+            this._startTime = Date.now();
+            var seconds = 0;
+            document.querySelector('nav .navbar-title').textContent = `Player: ${this._name}  Elapsed time:  ${seconds++}`;
+
+            this._timer = setInterval(() =>
+
+                document.querySelector('nav .navbar-title').textContent = `Player: ${this._name}   Elapsed time:  ${seconds++}`, 1000);
+        }
+        gotoScore() {
+            var timeElapsedInSeconds = Math.floor((Date.now() - this._startTime) / 1000);
+            clearInterval(this._timer);
+
+            setTimeout(() =>
+                // TODO Step 6: change path to: `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
+                window.location = `../score/score.component.html?name=${this._name}&size=${this._size}&time=${timeElapsedInSeconds}`, 750); // TODO Step 3.2: Why bind(this)?
+        }
+        fetchConfig(cb) {
+            var xhr = typeof XMLHttpRequest != 'undefined' ?
+                new XMLHttpRequest() :
+                new ActiveXObject('Microsoft.XMLHTTP');
+
+            xhr.open('get', `${environment.api.host}/board?size=${this._size}`, true);
+
+            xhr.onreadystatechange = () => {
+                var status;
+                var data;
+                // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
+                if (xhr.readyState == 4) { // `DONE`
+                    status = xhr.status;
+                    if (status == 200) {
+                        data = JSON.parse(xhr.responseText);
+                        cb(data);
+                    } else {
+                        throw new Error(status)
+                    }
+                }
+            };
+            xhr.send();
+        }
+        _flipCard(card) {
+            if (this._busy) {
+                return;
             }
 
-            this.start();
-        }).bind(this));
-    }
-
-    function start() {
-        this._startTime = Date.now();
-        var seconds = 0;
-        // TODO Step 3.2: use template literals
-        document.querySelector('nav .navbar-title').textContent = 'Player: ' + this._name + '. Elapsed time: ' + seconds++;
-
-        this._timer = setInterval(function() { // TODO Step 3.2: use arrow function
-            // TODO Step 3.2: use template literals
-            document.querySelector('nav .navbar-title').textContent = 'Player: ' + this._name + '. Elapsed time: ' + seconds++;
-        }.bind(this), 1000);
-    }
-
-    function gotoScore() {
-        var timeElapsedInSeconds = Math.floor((Date.now() - this._startTime) / 1000);
-        clearInterval(this._timer);
-
-        setTimeout(() => // TODO Step 3.2: use arrow function.
-            // TODO Step 3.2: use template literals
-            // TODO Step 6: change path to: `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
-            window.location = `../score/score.component.html?name=${this._name}&size=${this._size}&time=${timeElapsedInSeconds}`, 750); // TODO Step 3.2: Why bind(this)?
-    }
-
-    function fetchConfig(cb) {
-        var xhr = typeof XMLHttpRequest != 'undefined' ?
-            new XMLHttpRequest() :
-            new ActiveXObject('Microsoft.XMLHTTP');
-
-        // TODO Step 3.2 use template literals
-        xhr.open('get', environment.api.host + '/board?size=' + this._size, true);
-
-        // TODO Step 3.2 use arrow function
-        xhr.onreadystatechange = () => {
-            var status;
-            var data;
-            // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
-            if (xhr.readyState == 4) { // `DONE`
-                status = xhr.status;
-                if (status == 200) {
-                    data = JSON.parse(xhr.responseText);
-                    cb(data);
-                } else {
-                    throw new Error(status)
-                }
+            if (card.flipped) {
+                return;
             }
-        };
-        xhr.send();
-    }
-
-    function _flipCard(card) {
-        if (this._busy) {
-            return;
-        }
-
-        if (card.flipped) {
-            return;
-        }
 
 
-        // flip the card
-        card.flip();
+            // flip the card
+            card.flip();
 
-        // if flipped first card of the pair
-        if (!this._flippedCard) {
-            // keep this card flipped, and wait for the second card of the pair
-            this._flippedCard = card;
-        } else {
-            // second card of the pair flipped...
-
-            // if cards are the same
-            if (card.equals(this._flippedCard)) {
-                this._flippedCard.matched = true;
-                card.matched = true;
-                this._matchedPairs += 1;
-
-                // reset flipped card for the next turn.
-                this._flippedCard = null;
-
-                if (this._matchedPairs === this._size) {
-                    this.gotoScore();
-                }
+            // if flipped first card of the pair
+            if (!this._flippedCard) {
+                // keep this card flipped, and wait for the second card of the pair
+                this._flippedCard = card;
             } else {
-                this._busy = true;
+                // second card of the pair flipped...
 
-                // cards did not match
-                // wait a short amount of time before hiding both cards
-                // TODO Step 3.2 use arrow function
-                setTimeout(function() {
-                    // hide the cards
-                    this._flippedCard.flip();
-                    card.flip();
-                    this._busy = false;
+                // if cards are the same
+                if (card.equals(this._flippedCard)) {
+                    this._flippedCard.matched = true;
+                    card.matched = true;
+                    this._matchedPairs += 1;
 
                     // reset flipped card for the next turn.
                     this._flippedCard = null;
-                }.bind(this), 500);
+
+                    if (this._matchedPairs === this._size) {
+                        this.gotoScore();
+                    }
+                } else {
+                    this._busy = true;
+
+                    // cards did not match
+                    // wait a short amount of time before hiding both cards
+
+                    setTimeout(() => {
+                        // hide the cards
+                        this._flippedCard.flip();
+                        card.flip();
+                        this._busy = false;
+
+                        // reset flipped card for the next turn.
+                        this._flippedCard = null;
+                    }, 500);
+                }
             }
         }
     }
@@ -170,12 +147,8 @@
 
         let parts = query
             .split(delimiter);
-        // TODO Step 3.3: Use Array.map() & Array.reduce()
-        for (let i in parts) {
-            let item = parts[i];
-            let kv = item.split('=');
-            result[kv[0]] = kv[1];
-        }
+
+        parts.map(p => p.split('=').reduce((a, b) => result[a] = b))
 
         return result;
     }
